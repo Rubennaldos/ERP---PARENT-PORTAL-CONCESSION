35c4c7deeeeddd3f6f62fee0d8eac12be45c96e0
Rubennaldos <albertonaldos@gmail.com>
2026-02-04 02:10:14 -0500

    chore: Actualizar version a 1.9.0 - Mejoras en wizard de pedidos fisicos y correccion de zona horaria UTC

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,8 +1,8 @@
 export const APP_CONFIG = {
-  version: '1.12.9',
+  version: '1.9.0',
   status: 'PRODUCTION',
   appName: 'Lima Café 28',
   designedBy: 'ARQUISIA Soluciones',
-  fullVersion: 'v1.12.9',
+  fullVersion: 'v1.9.0',
 };
 
